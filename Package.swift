5906bf4712f0bb35bac80e69ceabef63b9c05a17
stakss <18610863095@163.com>
2023-02-21 15:29:38 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,17 @@ import PackageDescription
 
 let package = Package(
     name: "OTPKit",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "OTPKit", targets: ["OTPKit"])
+    ],
     dependencies: [
       .Package(url: "https://github.com/chrisamanse/CryptoKit.git", majorVersion: 0)
+    ],
+    targets: [
+        .target(name: "OTPKit", dependencies: ["CryptoKit"], path: "OTPKit/"),
+        .testTarget(name: "OTPKitTests", dependencies: ["OTPKit"])
     ]
 )
